d060b246d266438bee93ceea3b2b76ad1c70fb37
AleksandrFn <godldamn@mail.ru>
2022-11-08 16:17:41 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "51351351341313";
+        System.out.println(Arrays.toString(splitStringTwoParts(str)));
+    }
+
+    public static String[] splitStringTwoParts(String str) {
+        String[] parts = new String[2];
+        if (str.length() % 2 == 0) {
+            parts[0] = str.substring(0, str.length() / 2);
+            parts[1] = str.substring(str.length() / 2);
+        } else {
+            parts[0] = str.substring(0, str.length() / 2);
+            parts[1] = str.substring(str.length() / 2 + 1);
+        }
+
+        return parts;
     }
 }
